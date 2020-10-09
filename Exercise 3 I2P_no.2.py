ac7f1f74545496fba7ee54060af97488f1faef43
DarrenMannuela <70499659+DarrenMannuela@users.noreply.github.com>
2020-10-09 13:59:23 +0700

    Delete Exercise 3 I2P_no.2.py

--- Exercise 3 I2P_no.2.py
+++
@@ -1,9 +0,0 @@
-
-def calc_weight_on_planet(weight, gravity = 23.1):
-    earth_gravity = 9.8
-    mass = weight/earth_gravity
-    weight_on_planet = mass*gravity
-    print(weight_on_planet)
-
-
-calc_weight_on_planet(78,9.8)
